2ecbdeb19a1581ee2e8d1f072e51e3b021594568
Heri Kiswanto <heryoulie@gmail.com>
2018-11-17 14:12:03 +0700

    push repo

---
+++ app/src/main/java/kiswanto/heri/modul21/SecondActivity.java
@@ -0,0 +1,39 @@
+package kiswanto.heri.modul21;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "kiswanto.heri.modul21.extra.REPLY";
+    private EditText mReply;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(FirstActivity.EXTRA_MESSAGE);
+        TextView textView = findViewById(R.id.text_message);
+        mReply = findViewById(R.id.editText_second);
+
+        textView.setText(message);
+
+
+    }
+
+    public void returnReply(View view) {
+
+        String reply = mReply.getText().toString();
+
+        Intent replyIntent = new Intent();
+        replyIntent.putExtra(EXTRA_REPLY, reply);
+        setResult(RESULT_OK, replyIntent);
+        finish();
+    }
+}
